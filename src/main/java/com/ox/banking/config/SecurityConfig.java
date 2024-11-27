f6dfe3b8f2df0c50369d63c545ca02b4a75368f3
bombs <manuele.bombelli@gmail.com>
2024-11-27 14:41:30 +0100

    first commit

--- src/main/java/com/ox/banking/config/SecurityConfig.java
+++ src/main/java/com/ox/banking/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.ox.banking.config;
 
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -28,14 +31,25 @@ public class SecurityConfig {
 	    
 
 	    
-	    @Bean
-	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-	        http
-	            .cors().and()  // Abilita CORS
-	            .csrf().disable()
-	            .authorizeHttpRequests()
-	            .anyRequest().permitAll();
-	        return http.build();
-	    }
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .cors(cors -> cors.configurationSource(request -> {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(Arrays.asList("https://ox-frontend-47b0c222d86c.herokuapp.com"));
+                config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                config.setAllowedHeaders(Arrays.asList("*"));
+                config.setAllowCredentials(false);
+                return config;
+            }))
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth/**").permitAll()
+                .anyRequest().permitAll())
+            .sessionManagement(session -> 
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            );
+        return http.build();
+    }
 	
 }
